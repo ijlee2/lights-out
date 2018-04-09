af9a0a89a67c4f845bc3e657faa5e719d7126703
ijlee2 <ijlee2@users.noreply.github.com>
2018-04-08 21:14:28 -0500

    Step 2f. action complete

--- app/components/lights-out/component.js
+++ app/components/lights-out/component.js
@@ -49,16 +49,16 @@ export default Component.extend(ResponsiveMixin, {
         let buttons = [];
 
         for (let i = 0; i < this.get('numButtons.y'); i++) {
-            let rowsOfButtons = [];
+            let rowOfButtons = [];
 
             for (let j = 0; j < this.get('numButtons.x'); j++) {
-                rowsOfButtons.push({
+                rowOfButtons.push({
                     coordinates: {x: j, y: i},
                     isLightOn: false
                 });
             }
 
-            buttons.push(rowsOfButtons);
+            buttons.push(rowOfButtons);
         }
 
         this.set('buttons', buttons);
@@ -134,7 +134,7 @@ export default Component.extend(ResponsiveMixin, {
 
     areLightsOut: computed('buttons', function() {
         // If any of the lights are still on, the game continues
-        return !this.get('buttons').any(rowsOfButtons => rowsOfButtons.any(button => button.isLightOn));
+        return !this.get('buttons').any(rowOfButtons => rowOfButtons.any(button => button.isLightOn));
     }),
 
 
@@ -224,7 +224,7 @@ export default Component.extend(ResponsiveMixin, {
             .attr('class', 'buttons');
 
         // It's easier to work with 1D data in D3. Convert the 2D array to an 1D array.
-        const buttons = this.get('buttons').reduce((accumulator, rowsOfButtons) => accumulator.concat(rowsOfButtons), []);
+        const buttons = this.get('buttons').reduce((accumulator, rowOfButtons) => accumulator.concat(rowOfButtons), []);
 
         // Create buttons inside the buttons group
         let buttonGroup = select('.buttons')
